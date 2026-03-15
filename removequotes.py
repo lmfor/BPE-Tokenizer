ad594629bbb0d5948d532f5505c5b86ebdb6bd5e
lmfor <leonardo.e.schaub@gmail.com>
2026-03-15 16:06:45 -0500

    basic tokenization

--- removequotes.py
+++ removequotes.py
@@ -3,8 +3,10 @@
 def removequotes():
     text = input("Text: ")
     for c in text:
-        if c != "'":
+        if c != "'" or c!= "\"":
             print(c, end="")
+    
+    [print() for i in range(4)]
         
 
 if __name__ == "__main__":
